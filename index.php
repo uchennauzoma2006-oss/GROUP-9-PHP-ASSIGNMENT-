d2553f978b2b22cb46a2f2178567376deb16a558
NATHAN-XEX <macsukun3672@student.babcock.edu.ng>
2025-10-26 08:25:11 +0100

    Add files via upload
    Added login.php page

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <link rel="stylesheet" href="style.css">
+    <title>Student Enrollment Portal</title>
+</head>
+<body>
+<h1>Welcome to the Course Enrollment System</h1>
+<ul>
+    <li><a href="enroll.php">Enroll Student</a></li>
+    <li><a href="view_enrollments.php">View Enrollments</a></li>
+    <li><a href="admin.php">Admin Panel</a></li>
+    <li><a href="dashboard.php">Dashboard</a></li>
+</ul>
+</body>
+</html>
